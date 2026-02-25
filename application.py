a052b32dce3c76ec0b15987af61dcca0602a69bb
OmoJolasunMomi Bankole <D00215474@student.dkit.ie>
2026-02-25 22:21:52 +0000

    Triggering

--- application.py
+++ application.py
@@ -9,20 +9,27 @@ def display(data):
     print(f"\tQuantity in stock: {data[4]}")
     if len(data) == 7:
         print(f"\tGenre(s): {data[6]}")
+        
 
 # Ask user to enter a filename
-valid = False
-while not valid:
-    try:
-        filename = input("Please enter the inventory filename: ")
-        valid = True
-    except FileNotFoundError as e:
-            print("File is not found")
-    with open(filename, "r") as file_handle:
-        # Read content of file in and store in a list of content
-        for line in file_handle:
-            line = line.strip()
-            components = line.split("%%")
-            data = []
+filename = input("Please enter the inventory filename: ")
 
-        
+with open(filename, "r") as file_handle:
+    # Read content of file in and store in a list of content
+    for line in file_handle:
+        line = line.strip()
+        components = line.split("%%")
+        data = []
+        data.append(components[1])
+        data.append(components[2])
+        data.append(float(components[3]))
+        data.append(float(components[4]))
+        data.append(int(components[5]))
+        if components[0] == "Book":
+            data.append(components[6])
+            genres = components[7].split("&&")
+            data.append(genres)
+
+        # Display data for this line
+        display(data)
+

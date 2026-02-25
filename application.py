e6eaae4e65aab2a330f77eab8dad1d5b0653304c
OmoJolasunMomi Bankole <D00215474@student.dkit.ie>
2026-02-25 23:53:19 +0000

    Exercise A4) Granular/local exception handling (start)

--- application.py
+++ application.py
@@ -50,43 +50,5 @@ while file:
         parts = line.split("%%")
         product_info = []
 
-        try:
-            # Split the line into parts using the "%%" delimiter and store the product information in a list. If the line does not contain the expected number of parts, catch the exception and print an error message.
-            product_info.append(parts[1])
-            product_info.append(parts[2])
 
-            try: 
-                product_info.append(float(parts[3]))
-            except:
-                print("Error: Cost price is not a valid number. Skipping line.")
-                exit()
-            
-            try:
-                product_info.append(int(parts[4]))
-            except:
-                print("Error: Quantity in stock is not a valid number. Skipping line.")
-                exit()
-            
-            try:
-                product_info.append(float(parts[5]))
-            except:
-                print("Error: Retail price is not a valid number. Skipping line.")
-                exit()
-            
-            if parts[0] == "book":
-                try:
-                    product_info.append(parts[6])
-                    genre_list = parts[7].split("%%")
-                    product_info.append(genre_list)
-                except:
-                    print("Error: Book genre(s) is not in the expected format. Skipping line.")
-                    exit()
-                    
-            display(product_info) # display the product details using the display function
-
-        except IndexError:
-            print("Error: Line does not contain the expected number of parts. Skipping line.")
-            exit()
-            
-    print("End of file reached.")
 

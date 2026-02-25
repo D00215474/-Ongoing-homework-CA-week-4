1cfb8ab097db790ae2b5296994dbb0f8765b26a6
OmoJolasunMomi Bankole <D00215474@student.dkit.ie>
2026-02-25 23:31:04 +0000

    Exercise A3) Handling exceptions through reattempts update

--- application.py
+++ application.py
@@ -9,43 +9,39 @@ def display(data):
     print(f"\tQuantity in stock: {data[4]}")
     if len(data) == 7:
         print(f"\tGenre(s): {data[6]}")
-        
+
+filename = input("Please enter the inventory filename: ")
+
+# Initialize the number of tries and the total number of tries allowed.
 trys = 0
 total_tries = 5
 
+# Try to open the file and read the first line. If the file is not found, catch the exception and increment the number of tries. Calculate the number of tries left and print an error message. If there are no tries left, exit the program..
 while trys < total_tries:
     filename = input("Please enter the inventory filename: ")
 
     try:
-        with open(filename, "r") as file_handle:
-            # Read content of file in and store in a list of content
-            for line in file_handle:
-                line = line.strip()
-                if not line:
-                    continue
+        #try to open the file and read the first line
+        with open(filename, "r") as file:
+            print("File opened successfully.")
+            break
 
-                components = line.split("%%")
-                data = []
-
-                try:
-                    data.append(components[1])
-                    data.append(components[2])
-                    data.append(float(components[3]))
-                    data.append(float(components[4]))
-                    data.append(int(components[5]))
-                    if components[0] == "Book":
-                        data.append(components[6])
-                        genres = components[7].split("&&")
-                        data.append(genres)
-
-                except (IndexError, ValueError) as e:
-                    print("File Not Found")
-                    exit()
-
+# if the file is not found, catch the exception and increment the number of tries. Calculate the number of tries left and print an error message. If there are no tries left, exit the program.
     except FileNotFoundError:
-        print("File Not Found")
-        exit()
-        
-        display(data)
-        
+        trys = trys + 1
+        trys_left = total_tries - trys
 
+# if the file is not found, print an error message and the number of tries left. If there are no tries left, exit the program.
+        if trys_left > 0:
+            print("File not found. Please try again.")
+            print("you have", trys_left, "tries left.")
+        else:
+            print("File not found. No more tries left. Exiting program.")
+            exit()
+
+# If the file is opened successfully, read the file line by line and display the product details. Skip empty lines.
+while file:
+    for line in file:
+        line = line.strip()
+        if line == "": #skip empty lines
+            continue

27c67cb8a983bb59fe08246ca4de25e8489fe94d
D00215474@student.dkit.ie <D00215474@student.dkit.ie>
2026-03-12 19:15:35 +0000

    Feature[custom_exceptions.py] creating custom exceptions for the student class functions.

---
+++ Week6 Homework/custom_exceptions.py
@@ -0,0 +1,7 @@
+class InvalidIDError(Exception):
+    """Exception raised for invalid IDs."""
+    pass
+
+class InvalidGradeError(ValueError):
+    """Exception raised for invalid grades."""
+    pass
